4ca38fbe3d5ffa26a5d0621f3fc012e592b33bb2
carlos738 <carlosasl135@hotmail.com>
2023-12-31 21:42:33 -0300

    pokedex

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -18,7 +18,6 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
